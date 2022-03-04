14e87ea1342048a22a6b5e66334d979addb99c48
emicif <emanuela.cifolelli@gmail.com>
2022-03-04 15:15:20 +0100

    Input utente

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log('JS Ok')
+
+// Chiedo età all'utente
+const age = prompt('Quanti anni hai?');
+console.log('La mia età è ' + age);
+
+// Chiedo all'utente quanti km deve percorrere
+const km = prompt('Quanti km devi percorrere?');
+console.log('I Kilometri che devo pecorrere sono ' + km);
