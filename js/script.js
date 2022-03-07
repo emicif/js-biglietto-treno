466977585f79fe12983eadbd3d90e4efb7e94b33
emicif <emanuela.cifolelli@gmail.com>
2022-03-07 10:08:52 +0100

    end

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 console.log('JS Ok')
 
 // Chiedo età all'utente
-const age = prompt('Quanti anni hai?');
+const age = parseInt (prompt('Quanti anni hai?'));
 console.log('La mia età è ' + age);
 
 // Chiedo all'utente quanti km deve percorrere
-const km = prompt('Quanti km devi percorrere?');
+const km = parseInt( prompt('Quanti km devi percorrere?'));
 console.log('I Kilometri che devo pecorrere sono ' + km);
 
 
@@ -38,9 +38,7 @@ if(age < 18) {
     let varTry = ((tot*20) / 100);
     let varSconto = (tot - varTry);
     console.log('Il costo del tuo biglietto con lo sconto minorenni è di €' + (varSconto .toFixed(2)));
-}  else {
-    console.log('Il costo del tuo biglietto è di €' + tot)
-}
+} 
 
 
 // lo sconto del 40% > 65
@@ -56,7 +54,7 @@ if(age > 65) {
 } 
 
 
-
+document.getElementById('price').innerText = `Il prezzo è di € ${varSconto .toFixed(2)}`;
 
 
 /*if (age < 18) {
